5f7203fdd3ed60ee720d6194db77cb1b52dc32b6
XanDrDz <alexandr.dzyub@gmail.com>
2020-09-23 01:03:09 +0500

    min

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,10 +1,18 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimizer: [
+            new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})
+        ],
+        },
     output: {
         libraryTarget: 'var',
         library: 'Client'
@@ -18,11 +26,14 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
     },
     plugins: [
+        new MiniCssExtractPlugin({
+             filename: "[name].css" 
+            }),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
